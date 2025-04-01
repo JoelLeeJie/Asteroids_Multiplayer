b13c985e1410bba10f1cb33f067c630d530c49bd
yeetong.t <yeetong.t@digipen.edu>
2025-04-01 19:11:16 +0800

    Integrating Client.hpp cpp, into the game sln, some updated steps in the game

--- CSD1130_Asteroids/Include/Main.h
+++ CSD1130_Asteroids/Include/Main.h
@@ -25,13 +25,41 @@ extern double	g_appTime;
 // ---------------------------------------------------------------------------
 // includes
 
+
+
+
+
+#include "winsock2.h"		// ...or Winsock alone
+#include "ws2tcpip.h"		// getaddrinfo()
+
+ // Tell the Visual Studio linker to include the following library in linking.
+ // Alternatively, we could add this file to the linker command-line parameters,
+ // but including it in the source code simplifies the configuration.
+#pragma comment(lib, "ws2_32.lib")
+#include "Windows.h"		// Entire Win32 API...
+#include <iostream>			// cout, cerr
+#include <iomanip>
+
+//For Multi-threading
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+
+
+
+
+
+
+
 #include "AEEngine.h"
 #include "Math.h"
 
 #include "GameStateMgr.h"
 #include "GameState_Asteroids.h"
 #include "Collision.h"
-
+#include "Client.hpp"
 
 #endif
 
